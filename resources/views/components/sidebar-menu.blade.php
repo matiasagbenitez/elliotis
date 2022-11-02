75ff958ee7ee9c1abe743ab9760de737763a8d56
Matías <matiasagbenitez@gmail.com>
2022-11-02 20:16:36 -0300

    Se agregó el link de Concursos al sidebar

--- resources/views/components/sidebar-menu.blade.php
+++ resources/views/components/sidebar-menu.blade.php
@@ -280,6 +280,16 @@
                 </div>
             </div>
 
+            <br>
+            <span class="font-bold">Concursos</span>
+            <hr>
+
+            <a href="{{ route('admin.tenderings.index') }}"
+                class="block px-4 py-1 mt-2 text-sm font-semibold text-gray-900 rounded-lg dark-mode:bg-gray-700 dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
+                <i class="fas fa-star mr-2"></i>
+                Concursos
+            </a>
+
 
             {{-- SUBTÍTULO PRODUCTOS --}}
             <br>
